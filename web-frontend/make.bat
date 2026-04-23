c6d1ca0c26f3aa04913341ad47b8e44bfb807304
nekromantikko <jonahpaakkonen@gmail.com>
2026-04-23 17:04:23 +0300

    Rename wasm module

--- web-frontend/make.bat
+++ web-frontend/make.bat
@@ -1,2 +1,2 @@
 @echo off
-emcc cpp/test.cpp -o src/wasm/test.js -sEXPORT_ES6=1 "-sEXPORT_NAME=test" -sENVIRONMENT=web "-sEXPORTED_RUNTIME_METHODS=ccall,cwrap" -I cpp/include/glm
+emcc cpp/ascii-renderer.cpp -o src/wasm/ascii-renderer.js -sEXPORT_ES6=1 "-sEXPORT_NAME=asciiRenderer" -sENVIRONMENT=web "-sEXPORTED_RUNTIME_METHODS=ccall,cwrap" -I cpp/include/glm
