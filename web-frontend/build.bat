29890b71477deb79d67fc758b4454125bbb0f9eb
nekromantikko <jonahpaakkonen@gmail.com>
2026-04-24 12:12:38 +0300

    Switch to cmake for wasm build

---
+++ web-frontend/build.bat
@@ -0,0 +1,3 @@
+@echo off
+cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+cmake --build build
--- web-frontend/make.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-emcc cpp/ascii-renderer.cpp -o src/wasm/ascii-renderer.js -sEXPORT_ES6=1 "-sEXPORT_NAME=asciiRenderer" -sENVIRONMENT=web "-sEXPORTED_RUNTIME_METHODS=ccall,cwrap" -I cpp/include/glm
