fcc467dd2e8cd517ec056e1787fd20ab8a00c13d
nekromantikko <jonahpaakkonen@gmail.com>
2026-06-29 13:20:27 +0300

    Fam: Replace unicode arrows with svg

--- web-frontend/src/fam/components/Stepper.tsx
+++ web-frontend/src/fam/components/Stepper.tsx
@@ -12,12 +12,37 @@ type StepperProps = {
     disabled?: boolean
 };
 
+const ArrowLeftIcon = ({ className = 'w-8 h-8 text-olive-300'}: { className?: string}) => (
+    <svg 
+        viewBox="0 0 24 24" 
+        className={className} 
+        fill="currentColor" 
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M14 7l-5 5 5 5V7z" />
+    </svg>
+);
+
+const ArrowRightIcon = ({ className = 'w-8 h-8 text-olive-300' }: { className?: string }) => (
+    <svg 
+        viewBox="0 0 24 24" 
+        className={className} 
+        fill="currentColor" 
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M10 17l5-5-5-5v10z" />
+    </svg>
+);
+
 export const Stepper = memo(({ label = '', value = 0, length = 4, onIncrement, onDecrement, disabled }: StepperProps) => {
-    const labelStr = `⮜ ${label} ⮞`;
     
     return (
         <div className="flex flex-col items-center m-2">
-            <Label>{labelStr}</Label>
+            <div className="flex flex-row items-center">
+                <ArrowLeftIcon />
+                <Label>{label}</Label>
+                <ArrowRightIcon />
+            </div>
             <div className="flex flex-row gap-2">
                 <TactileButton shape='rect' onPress={onDecrement} disabled={disabled} />
                 <DigitalDisplay value={value} length={length} disabled={disabled} />
